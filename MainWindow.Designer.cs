db5e1e87c97acef869a1af4717aaba56bbb37399
TheMrDH <128097665+TheMrDH@users.noreply.github.com>
2023-03-18 14:47:04 -0500

    Clearing

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Stt_to_tts
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbOutputList = new System.Windows.Forms.ComboBox();
+            this.cbInputList = new System.Windows.Forms.ComboBox();
+            this.tbTtsEntry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btSttStop = new System.Windows.Forms.Button();
+            this.btSttStart = new System.Windows.Forms.Button();
+            this.btTtsPlay = new System.Windows.Forms.Button();
+            this.btTtsStop = new System.Windows.Forms.Button();
+            this.tbSttResults = new System.Windows.Forms.TextBox();
+            this.plSttThreadIndicator = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // cbOutputList
+            // 
+            this.cbOutputList.FormattingEnabled = true;
+            this.cbOutputList.Location = new System.Drawing.Point(13, 38);
+            this.cbOutputList.Name = "cbOutputList";
+            this.cbOutputList.Size = new System.Drawing.Size(192, 21);
+            this.cbOutputList.TabIndex = 0;
+            this.cbOutputList.SelectedIndexChanged += new System.EventHandler(this.cbOutputList_SelectedIndexChanged);
+            // 
+            // cbInputList
+            // 
+            this.cbInputList.FormattingEnabled = true;
+            this.cbInputList.Location = new System.Drawing.Point(250, 37);
+            this.cbInputList.Name = "cbInputList";
+            this.cbInputList.Size = new System.Drawing.Size(205, 21);
+            this.cbInputList.TabIndex = 1;
+            this.cbInputList.SelectedIndexChanged += new System.EventHandler(this.cbInputList_SelectedIndexChanged);
+            // 
+            // tbTtsEntry
+            // 
+            this.tbTtsEntry.AcceptsReturn = true;
+            this.tbTtsEntry.AcceptsTab = true;
+            this.tbTtsEntry.Location = new System.Drawing.Point(13, 66);
+            this.tbTtsEntry.Multiline = true;
+            this.tbTtsEntry.Name = "tbTtsEntry";
+            this.tbTtsEntry.Size = new System.Drawing.Size(442, 190);
+            this.tbTtsEntry.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Audio Mirror Device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Audio Input Device";
+            // 
+            // btSttStop
+            // 
+            this.btSttStop.Location = new System.Drawing.Point(379, 289);
+            this.btSttStop.Name = "btSttStop";
+            this.btSttStop.Size = new System.Drawing.Size(75, 23);
+            this.btSttStop.TabIndex = 5;
+            this.btSttStop.Text = "STT Stop";
+            this.btSttStop.UseVisualStyleBackColor = true;
+            this.btSttStop.Click += new System.EventHandler(this.btSttStop_Click);
+            // 
+            // btSttStart
+            // 
+            this.btSttStart.Location = new System.Drawing.Point(380, 260);
+            this.btSttStart.Name = "btSttStart";
+            this.btSttStart.Size = new System.Drawing.Size(75, 23);
+            this.btSttStart.TabIndex = 6;
+            this.btSttStart.Text = "STT Start";
+            this.btSttStart.UseVisualStyleBackColor = true;
+            this.btSttStart.Click += new System.EventHandler(this.btSttStart_Click);
+            // 
+            // btTtsPlay
+            // 
+            this.btTtsPlay.Location = new System.Drawing.Point(299, 260);
+            this.btTtsPlay.Name = "btTtsPlay";
+            this.btTtsPlay.Size = new System.Drawing.Size(75, 23);
+            this.btTtsPlay.TabIndex = 7;
+            this.btTtsPlay.Text = "TTS Play";
+            this.btTtsPlay.UseVisualStyleBackColor = true;
+            this.btTtsPlay.Click += new System.EventHandler(this.btTtsPlay_Click);
+            // 
+            // btTtsStop
+            // 
+            this.btTtsStop.Location = new System.Drawing.Point(298, 289);
+            this.btTtsStop.Name = "btTtsStop";
+            this.btTtsStop.Size = new System.Drawing.Size(75, 23);
+            this.btTtsStop.TabIndex = 8;
+            this.btTtsStop.Text = "TTS Stop";
+            this.btTtsStop.UseVisualStyleBackColor = true;
+            this.btTtsStop.Click += new System.EventHandler(this.btTtsStop_Click);
+            // 
+            // tbSttResults
+            // 
+            this.tbSttResults.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.tbSttResults.Location = new System.Drawing.Point(13, 263);
+            this.tbSttResults.Multiline = true;
+            this.tbSttResults.Name = "tbSttResults";
+            this.tbSttResults.ReadOnly = true;
+            this.tbSttResults.Size = new System.Drawing.Size(233, 49);
+            this.tbSttResults.TabIndex = 9;
+            // 
+            // plSttThreadIndicator
+            // 
+            this.plSttThreadIndicator.BackColor = System.Drawing.Color.Maroon;
+            this.plSttThreadIndicator.Location = new System.Drawing.Point(252, 263);
+            this.plSttThreadIndicator.Name = "plSttThreadIndicator";
+            this.plSttThreadIndicator.Size = new System.Drawing.Size(41, 49);
+            this.plSttThreadIndicator.TabIndex = 10;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 317);
+            this.Controls.Add(this.plSttThreadIndicator);
+            this.Controls.Add(this.tbSttResults);
+            this.Controls.Add(this.btTtsStop);
+            this.Controls.Add(this.btTtsPlay);
+            this.Controls.Add(this.btSttStart);
+            this.Controls.Add(this.btSttStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTtsEntry);
+            this.Controls.Add(this.cbInputList);
+            this.Controls.Add(this.cbOutputList);
+            this.Name = "MainWindow";
+            this.Text = "Speech To Text To Speech";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbOutputList;
+        private System.Windows.Forms.ComboBox cbInputList;
+        private System.Windows.Forms.TextBox tbTtsEntry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btSttStop;
+        private System.Windows.Forms.Button btSttStart;
+        private System.Windows.Forms.Button btTtsPlay;
+        private System.Windows.Forms.Button btTtsStop;
+        private System.Windows.Forms.TextBox tbSttResults;
+        private System.Windows.Forms.Panel plSttThreadIndicator;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+
+namespace Stt_to_tts
+{
+    public partial class MainWindow : Form
+    {
+        private delegate void TextDelegate(string text);
+        private SpeechSynthesizer synthesizer;
+        private SpeechSynthesizer memorySynthesizer;
+        private SpeechRecognitionEngine recognizer;
+        private List<string> playQueue;
+        private WaveOut waveout;
+        private WaveIn wavein; 
+        private BufferedWaveProvider wavestream;
+        private Thread threadSpeakRequest;
+        private Thread threadSpeakRecognition;
+        private bool closed;
+        private bool canceled;
+        private bool completed;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            closed = false;
+            canceled = true;
+            playQueue = new List<string>();
+            synthesizer = new SpeechSynthesizer();
+            memorySynthesizer = new SpeechSynthesizer();
+            wavestream = new BufferedWaveProvider(new WaveFormat(16000, 1));
+            wavestream.DiscardOnBufferOverflow = true;
+            for (int i = -1; i < WaveOut.DeviceCount; i++)
+            {
+                var temp = WaveOut.GetCapabilities(i);
+
+                cbOutputList.Items.Add($"{i} : {temp.ProductName}");
+                // populates drop down menu with audio devices
+            }
+            cbOutputList.Items.Add("None");
+            for (int i = -1; i < WaveIn.DeviceCount; i++)
+            {
+                var temp = WaveIn.GetCapabilities(i);
+
+                cbInputList.Items.Add($"{i} : {temp.ProductName}");
+                // populates drop down menu with audio devices
+            }
+            cbInputList.Items.Add("None");
+            threadSpeakRecognition = new Thread(remoteRec);
+            threadSpeakRequest = new Thread(remotePlayLoop);
+        }
+
+        private void cbOutputList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            ComboBox cbTemp = (ComboBox)sender;
+            int selection = cbTemp.SelectedIndex - 1;
+            if (selection == cbTemp.Items.Count - 2) // return if none selected
+                return;
+            waveout = new WaveOut();
+            waveout.DeviceNumber = selection;
+            Console.WriteLine(selection);
+        }
+
+        private void cbInputList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cbTemp = (ComboBox)sender;
+            int selection = cbTemp.SelectedIndex - 1;
+            if (selection == cbTemp.Items.Count - 2) // return if none selected
+                return;
+            wavein = new WaveIn { WaveFormat = new WaveFormat(16000, 1) };
+            wavein.DeviceNumber = selection;
+            Console.WriteLine(selection);
+            
+        }
+
+        private void speakAudio(string text) // makes it easy to call this from more sources
+        {
+            if (waveout != null)
+            {
+                SpeechAudioFormatInfo speechAudioFormatInfo = new SpeechAudioFormatInfo(16000, AudioBitsPerSample.Sixteen, AudioChannel.Mono);
+                Stream memoryStream = new MemoryStream();
+                IWaveProvider provider = new RawSourceWaveStream(memoryStream, new WaveFormat(16000, 16, 1));
+                memorySynthesizer.SetOutputToAudioStream(memoryStream, speechAudioFormatInfo);
+                memorySynthesizer.Speak(text);
+                memorySynthesizer.SetOutputToNull();
+                memoryStream.Position = 0; // reset stream position to 0
+                waveout.Init(provider);
+                waveout.Play();
+                Console.WriteLine("Not Null");
+                Console.WriteLine(memoryStream.Length);
+                Thread thread = new Thread(() => waitForPlayFinish(memoryStream));
+                thread.Start();
+
+            }
+            synthesizer.SpeakAsync(text);
+        }
+
+
+
+        private void btTtsPlay_Click(object sender, EventArgs e)
+        {
+            speakAudio(tbTtsEntry.Text);
+           
+        }
+
+        private void waitForPlayFinish(Stream stream)
+        {
+            while(waveout.PlaybackState != PlaybackState.Stopped)
+            {
+                Thread.Sleep(300);
+                //Console.WriteLine(waveout.PlaybackState);
+            }
+            //Console.WriteLine("Stopped");
+            stream.Close();// be sure to close stream when done to prevent funky stuff
+        }
+
+
+        private void btTtsStop_Click(object sender, EventArgs e)
+        {
+            synthesizer.SpeakAsyncCancelAll();
+            if(waveout != null)
+            {
+                waveout.Stop();
+            }
+        }
+
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            synthesizer.SpeakAsyncCancelAll();
+            closed = true;
+        }
+
+        private void btSttStart_Click(object sender, EventArgs e)
+        {
+            if(cbInputList.SelectedIndex - 1 == cbInputList.Items.Count - 2 || cbInputList.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select input device", "Input Device Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (threadSpeakRecognition.IsAlive)
+            {
+                MessageBox.Show("Stt is already running.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            plSttThreadIndicator.BackColor = Color.Green;
+            canceled = false;
+
+            threadSpeakRequest.Start();
+            threadSpeakRecognition.Start();
+            
+        }
+
+        private void btSttStop_Click(object sender, EventArgs e)
+        {
+            canceled = true;
+            plSttThreadIndicator.BackColor = Color.Maroon;
+        }
+
+        private void WriteSafe(string text) // show what text it has found in form safely
+        {
+            if (tbSttResults.InvokeRequired)
+            {
+                var d = new TextDelegate(WriteSafe);
+                tbSttResults.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                tbSttResults.Text = text;
+            }
+        }
+
+
+        private void remotePlayLoop()
+        {
+            while (!closed && !canceled)
+            {
+                if (playQueue.Count > 0)
+                {
+                    // Console.WriteLine("Readout" + readOuts[0]);
+                    speakAudio(playQueue[0]);
+                    playQueue.RemoveAt(0);
+                    playQueue.TrimExcess();
+                }
+
+                Thread.Sleep(100);
+            }
+
+            threadSpeakRequest = new Thread(remotePlayLoop);
+        }
+
+        private void remoteRec()
+        {
+            Stream buffStream = new SpeechStreamer(4096);
+            wavein.StartRecording();
+            wavein.DataAvailable += (s, a) =>
+            {
+                buffStream.Write(a.Buffer, 0, a.BytesRecorded); // constantly write to beginning of speechstream to allow for continuous live recognition
+            };
+            completed = false;
+            // Create an in-process speech recognizer for the en-US locale.  
+            using (
+            recognizer = new SpeechRecognitionEngine(new CultureInfo("en-US")))
+            {
+
+                // Create and load a dictation grammar.  
+                recognizer.LoadGrammar(new DictationGrammar());
+
+                // Add a handler for the speech recognized event.  
+                recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(SpeechRecognizedHandler);
+                recognizer.RecognizeCompleted += (o, e) =>
+                {
+                   /* if (e.Error != null)
+                    {
+                        Console.WriteLine("Error occurred during recognition: {0}", e.Error);
+                    }
+                    else if (e.InitialSilenceTimeout)
+                    {
+                        Console.WriteLine("Detected silence");
+                    }
+                    else if (e.BabbleTimeout)
+                    {
+                        Console.WriteLine("Detected babbling");
+                    }
+                    else if (e.InputStreamEnded)
+                    {
+                        Console.WriteLine("Input stream ended early");
+                    }
+                    else if (e.Result != null)
+                    {
+                        Console.WriteLine("Grammar = {0}; Text = {1}; Confidence = {2}", e.Result.Grammar.Name, e.Result.Text, e.Result.Confidence);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No result");
+                    }
+                   */
+                    completed = true;
+                };
+                // set input to constantly re-written stream.
+                recognizer.SetInputToAudioStream(buffStream, new SpeechAudioFormatInfo(16000, AudioBitsPerSample.Sixteen, AudioChannel.Mono));
+
+                // Start asynchronous, continuous speech recognition.  
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+
+                while (!completed) // this keeps the thread alive
+                {
+                    if (canceled)
+                    {
+                        wavein.StopRecording();
+                        buffStream.Close();// you must close the stream before trying to cancel the async recognition. Else it freezes.
+                        recognizer.RecognizeAsyncCancel();
+                        threadSpeakRecognition = new Thread(remoteRec);
+                        buffStream  = new SpeechStreamer(4096); // I need to do this to make it so one is able to restart the thread, else you get a _writeEvent = null error. No clue why.
+                        break;
+                    }
+                    if (closed)
+                    {
+                        wavein.StopRecording();
+                        buffStream.Close(); // you must close the stream before trying to cancel the async recognition. Else it freezes.
+                        recognizer.RecognizeAsyncCancel();
+                        threadSpeakRecognition = new Thread(remoteRec);
+                        break;
+                    }
+                    Thread.Sleep(600);
+                }
+                //recognizer.RecognizeAsyncCancel(); // kill any recognition stuff (it tends to freeze)
+            }
+
+        }
+        void SpeechRecognizedHandler(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result != null && e.Result.Text != null)
+            {
+                //Console.WriteLine("  Recognized text =  {0}", e.Result.Text);
+                string temp = e.Result.Text;
+                WriteSafe(temp);
+                playQueue.Add(temp); // add strings to readout list, to create a basic queue system
+            }
+            else
+            {
+                Console.WriteLine("  Recognized text not available.");
+            }
+        }
+
+        void RecognizeCompletedHandler(object sender, RecognizeCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Console.WriteLine("  Error encountered, {0}: {1}",
+                  e.Error.GetType().Name, e.Error.Message);
+            }
+            if (e.Cancelled)
+            {
+                Console.WriteLine("  Operation cancelled.");
+            }
+            if (e.InputStreamEnded)
+            {
+                Console.WriteLine("  End of stream encountered.");
+            }
+
+            //completed = true;
+        }
+
+
+    }
+
+
+
+
+    class SpeechStreamer : Stream // custom stream wrap to create a cyclical buffer, thanks Sean from stack overflow! https://stackoverflow.com/questions/1682902/streaming-input-to-system-speech-recognition-speechrecognitionengine
+    {
+        private AutoResetEvent _writeEvent;
+        private List<byte> _buffer;
+        private int _buffersize;
+        private int _readposition;
+        private int _writeposition;
+        private bool _reset;
+
+        public SpeechStreamer(int bufferSize)
+        {
+            _writeEvent = new AutoResetEvent(false);
+            _buffersize = bufferSize;
+            _buffer = new List<byte>(_buffersize);
+            for (int i = 0; i < _buffersize; i++)
+                _buffer.Add(new byte());
+            _readposition = 0;
+            _writeposition = 0;
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override long Length
+        {
+            get { return -1L; }
+        }
+
+        public override long Position
+        {
+            get { return 0L; }
+            set { }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return 0L;
+        }
+
+        public override void SetLength(long value)
+        {
+
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int i = 0;
+            while (i < count && _writeEvent != null)
+            {
+                if (!_reset && _readposition >= _writeposition)
+                {
+                    _writeEvent.WaitOne(100, true);
+                    continue;
+                }
+                buffer[i] = _buffer[_readposition + offset];
+                _readposition++;
+                if (_readposition == _buffersize)
+                {
+                    _readposition = 0;
+                    _reset = false;
+                }
+                i++;
+            }
+
+            return count;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            for (int i = offset; i < offset + count; i++)
+            {
+                _buffer[_writeposition] = buffer[i];
+                _writeposition++;
+                if (_writeposition == _buffersize)
+                {
+                    _writeposition = 0;
+                    _reset = true;
+                }
+            }
+            _writeEvent.Set();
+
+        }
+
+        public override void Close()
+        {
+            _writeEvent.Close();
+            _writeEvent = null;
+            base.Close();
+        }
+
+        public override void Flush()
+        {
+
+        }
+    }
+}
